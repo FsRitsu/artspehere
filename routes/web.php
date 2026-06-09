27ec50596140e9498aabd840a87cabecaa18a4a2
Fikri Rizal H <fikri.rizal@widyatama.ac.id>
2026-06-09 13:44:26 +0700

    test: add registration files -F

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+
+Route::get('/registrasi', [AuthController::class, 'showRegistration']) ->name('registrasi.show');
 
 Route::get('/', function () {
     return view('testcard');
